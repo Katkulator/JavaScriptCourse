27f78e2dfb3d37a3532a867c0792f973e0f8c9d3
Katkulator <matkokatic92@gmail.com>
2024-01-25 15:50:13 +0100

    add fetch star wars api

--- JavaScript/AjaxAndAPIs/FetchAPI/app.js
+++ JavaScript/AjaxAndAPIs/FetchAPI/app.js
@@ -1,7 +1,36 @@
-fetch("https://swapi.dev/api/people/1/")
-    .then(res => {
-        console.log("Resolved", res)
-    })
-    .catch(err => {
+// fetch("https://swapi.dev/api/people/1/") //fetch sends request to this url and returns a promise which can be resolved or rejected
+//     .then(res => {
+//         console.log("Resolved", res);
+//         return res.json()  //.json also return a promise
+//     })
+//     .then((data) => {
+//         console.log("JSON DONE", data);
+//         return fetch("https://swapi.dev/api/people/2/");
+//     })
+//     .then((res) => {
+//         console.log("Second request!")
+//         return res.json();
+//     })
+//     .then((data) => {
+//         console.log(data)
+//     })
+//     .catch(err => {
+//         console.log("Error!", err)
+//     })
+
+const loadStarWarsPeople = async () => {
+    try{
+        let response = await fetch("https://swapi.dev/api/people/1/")
+        let data = await response.json()
+        console.log(data)
+        response = await fetch("https://swapi.dev/api/people/2/")
+        data = await response.json()
+        console.log(data)
+    } 
+    catch (err) {
         console.log("Error!", err)
-    })
+    }
+
+}
+
+loadStarWarsPeople();
