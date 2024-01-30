5cf507b0014a6a3f41d45091016f89d09775ba7e
Matko Katić <matkokatic92@gmail.com>
2024-01-30 21:03:55 +0100

    add ShoppingList

--- React/first-react-app/src/App.jsx
+++ React/first-react-app/src/App.jsx
@@ -7,9 +7,18 @@ import DoubleDice from './DoubleDie';
 import Heading from './Heading';
 import ColorList from './ColorList';
 import Slots from './Slots';
+import ShoppingList from './ShoppingList';
+
+const data = [
+  { id: 1, item: "Eggs", quantity: 12, completed: false },
+  { id: 2, item: "Milk", quantity: 2, completed: true },
+  { id: 3, item: "Bread", quantity: 1, completed: false },
+  { id: 4, item: "Pasta", quantity: 3, completed: false },
+  { id: 5, item: "Vegetables", quantity: 7, completed: true }
+]
 
 function App() {
-  return(
+  return (
     <div>
       {/* <Heading  color='magenta' text = "im a heading" fontSize="48px"/>
       <Heading  color='teal' text = "im a heading too" fontSize="20px"/>
@@ -21,11 +30,13 @@ function App() {
       <DoubleDice />
       <DoubleDice />
       <ColorList colors = {["red", "blue", "green", "purple"]} /> */}
-      <Slots val1="🍒" val2="🍌" val3="🍊" />
+      {/* <Slots val1="🍒" val2="🍌" val3="🍊" />
+      <Slots val1="🍒" val2="🍒" val3="🍒" /> */}
+      <ShoppingList items={data} />
     </div>
-    
+
   );
-  
+
 }
 
 export default App;
--- React/first-react-app/src/Slots.jsx
+++ React/first-react-app/src/Slots.jsx
@@ -1,21 +1,36 @@
-export default function Slots() {
-    const slot = () => {
-       const num = Math.floor(Math.random() * 3) + 1;
-       switch (num){
-        case 1 :
-            return "🍒"
-        case 2:
-            return "🍌"
-        case 3:
-            return "🍊"
-       }
-    
-    }
+// export default function Slots() {
+//     const slot = () => {
+//         const num = Math.floor(Math.random() * 3) + 1;
+//         switch (num) {
+//             case 1:
+//                 return "🍒"
+//             case 2:
+//                 return "🍌"
+//             case 3:
+//                 return "🍊"
+//         }
 
+//     }
+
+//     return (
+//         <div>
+//             <p>{slot()}{slot()}{slot()}</p>
+//             <p>{slot()}{slot()}{slot()}</p>
+//             <p>{slot()}{slot()}{slot()}</p>
+//         </div>
+//     );
+// }
+
+export default function Slots({ val1, val2, val3 }) {
+    const isWinner = val1 === val2 && val1 === val3
+    const styles = { color: isWinner ? "green" : "red" };
     return (
         <div>
-            <p>{slot()}{slot()}{slot()}</p>
+            <h1>
+                {val1} {val2} {val3}
+            </h1>
+            <h2 style={styles}>{isWinner ? "You win!" : "You loose!"}</h2>
+            {isWinner && <h3>Congrats!</h3>}
         </div>
     );
 }
-
