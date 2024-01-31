f5cac8966358d146c88be4e88f3b9d5fd71b90f3
Katkulator <matkokatic92@gmail.com>
2024-01-31 10:17:11 +0100

    add Property List practice

--- React/first-react-app/src/App.jsx
+++ React/first-react-app/src/App.jsx
@@ -8,6 +8,7 @@ import Heading from './Heading';
 import ColorList from './ColorList';
 import Slots from './Slots';
 import ShoppingList from './ShoppingList';
+import PropertyList from './PropertyList'
 
 const data = [
   { id: 1, item: "Eggs", quantity: 12, completed: false },
@@ -15,7 +16,16 @@ const data = [
   { id: 3, item: "Bread", quantity: 1, completed: false },
   { id: 4, item: "Pasta", quantity: 3, completed: false },
   { id: 5, item: "Vegetables", quantity: 7, completed: true }
-]
+];
+
+const properties = [
+  { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
+  { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
+  { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
+  { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
+  { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
+  { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
+];
 
 function App() {
   return (
@@ -32,7 +42,8 @@ function App() {
       <ColorList colors = {["red", "blue", "green", "purple"]} /> */}
       {/* <Slots val1="🍒" val2="🍌" val3="🍊" />
       <Slots val1="🍒" val2="🍒" val3="🍒" /> */}
-      <ShoppingList items={data} />
+      {/* <ShoppingList items={data} /> */}
+      <PropertyList properties = {properties} />
     </div>
 
   );
--- React/first-react-app/src/ShoppingList.jsx
+++ React/first-react-app/src/ShoppingList.jsx
@@ -1,14 +1,12 @@
+import ShoppingListItem from "./ShoppingListItem";
+
 export default function ShoppingList({ items }) {
     return (
         <ul>
-            {items.map((i) =>
-                <li key={i.id}
-                    style={{
-                        color: i.completed ? "grey" : "red",
-                        textDecoration: i.completed ? "line-through" : "none"
-                    }}>
-                    {i.item} - {i.quantity}
-                </li>)}
+            {items.map((i) => (
+                <ShoppingListItem
+                key={i.id} {...i}/>
+            ))}
         </ul>
     );
 }
