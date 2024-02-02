3c7ecd4a0730c107c90f594b13c923b23ee4e8a8
Katkulator <matkokatic92@gmail.com>
2024-02-02 11:40:46 +0100

    finish state init function exercise

--- React/state-demo/src/Counter.jsx
+++ React/state-demo/src/Counter.jsx
@@ -1,16 +1,18 @@
 import { useState } from "react"
 
 export default function Counter() {
+    console.log("RENDERED!")
     const [count, setCount] = useState(0)
     const addOne = () => setCount(count + 1);
     // correct way to update state anytime state depends on the old version
     const addThree = () => setCount(currentCount => currentCount + 3); 
-
+    const setToTen = () => setCount(10)
     return (
         <div>
             <p>Count: {count}</p>
             <button onClick={addOne}>+1</button>
             <button onClick={addThree}>+3</button>
+            <button onClick={setToTen}>Set to 10</button>
         </div>
     );
 }
--- React/state-demo/src/StateInitFunc.jsx
+++ React/state-demo/src/StateInitFunc.jsx
@@ -6,10 +6,17 @@ function generateGameBoard() {
 }
 
 export default function StateInitFunc () {
-    const [board, setBoard] = useState(generateGameBoard)
+    // if you need to set the initial state using some initial state generating function, instead of executing it,
+    // you can just pass the function - useState(generateGameBoard) instead of - useState(generateGameBoard())
+    // as long as you pass the function in useState, react will take the function, execute it once, take the return value
+    // and use it as initial state and on future renders it will just ignore it.
+    // useState(generateGameBoard()) is run everytime the component renders
+    // useStaet(generateGameBoard) is run only one time
+    const [board, setBoard] = useState(generateGameBoard);
+
     return (
         <div>
-            <button>Click me to change State</button>
+            <button onClick={() => setBoard("Hello")}>Click me to change State</button>
         </div>
     );
 }
