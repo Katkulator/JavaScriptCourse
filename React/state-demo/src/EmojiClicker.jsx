405e797fc9f550241fdbc2cd09118fd7890424d5
Katkulator <matkokatic92@gmail.com>
2024-02-02 16:35:20 +0100

    add uuid lib and finish emoji clicker

--- React/state-demo/src/EmojiClicker.jsx
+++ React/state-demo/src/EmojiClicker.jsx
@@ -1,24 +1,37 @@
+import  {v4 as uuid}  from 'uuid'
 import { useState } from "react";
 
 
 
 export default function EmojyClicker () {
-    const emojiArr = ["🤯", "😄", "🥶", "😆", "🥳"]
-    const [emojis, setEmojis] = useState(["😀", ])
+
+    const emojiArr = [
+        {id: uuid(), emoji:"🤯"},
+        {id: uuid(), emoji:"😄"},
+        {id: uuid(), emoji:"🥶"},
+        {id: uuid(), emoji:"😆"},
+        {id: uuid(), emoji:"🥳"},
+    ];
+
+    const [emojis, setEmojis] = useState([{id: uuid(), emoji:"😀"}])
+    
     const selectEmoji = (arr) => {
         const randIndex = Math.floor(Math.random() * arr.length);
         return arr[randIndex]
     }
+    
     const addEmoji = () => {
        setEmojis((oldEmojis) => [...oldEmojis, selectEmoji(emojiArr)]) //...emojis makes a copy of an old array and adds in "🤯"
     }
     return (
         <div>
             {emojis.map((e) => 
-            <span style={{fontSize: "5rem"}}>{e}</span> 
+            <span key={e.id} style={{fontSize: "5rem"}}>{e.emoji}</span> 
             )}<br />
             <button onClick={addEmoji}>Add Emoji</button>
         </div>
     );
 }
+
+
 
