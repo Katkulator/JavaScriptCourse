9cccae8b0eba9f2aba56f56be44711429e7c6816
matko <matkokatic92@gmail.com>
2024-02-27 14:23:34 +0100

    finish react forms

--- React/react_forms/src/App.jsx
+++ React/react_forms/src/App.jsx
@@ -1,6 +1,4 @@
 import './App.css'
-import ShoppingListForm from './ShoppingListForm';
-import BetterSignupForm from './BetterSignupForm';
 import ShoppingList from './ShoppingList';
 
 function App() {
--- React/react_forms/src/ShoppingList.jsx
+++ React/react_forms/src/ShoppingList.jsx
@@ -1,64 +1,65 @@
 import { useState, useEffect } from "react"
 import { v4 as uuid } from 'uuid'
-import ShoppingListForm from "./ShoppingListForm";
+import ShoppingListForm from "./ShoppingListForm"
 import "./ShoppingList.css"
+
 
 function ShoppingList() {
 
-    const [items, setItems] = useState([]);
+  const [items, setItems] = useState([]);
 
-    const addItem = (item) => {
-        setItems(currItems => {
-            return [
-                ...currItems, { ...item, id: uuid() }];
-        })
+  const addItem = (item) => {
+    setItems(currItems => {
+      return [
+        ...currItems, { ...item, id: uuid(), product: item.product.trim() }]
+    })
+  }
+
+  const handleEsc = (evt) => {
+    if (evt.key === "Escape") {
+      setItems(oldItems => {
+        const updatedItems = [...oldItems]
+        updatedItems.pop()
+        return updatedItems
+      })
     }
+  }
 
-    const handleEsc = (evt) => {
-        if (evt.key === "Escape") {
-            setItems(oldItems => {
-                const updatedItems = [...oldItems]
-                updatedItems.pop()
-                return updatedItems
-            })
-        }
+  useEffect(() => {
+    document.addEventListener("keydown", handleEsc)
+    return () => {
+      document.removeEventListener("keydown", handleEsc)
     }
+  }, [])
 
-    useEffect(() => {
-        document.addEventListener("keydown", handleEsc)
-        return () => {
-            document.removeEventListener("keydown", handleEsc)
-        }
-    }, [])
-
-    const deleteItem = (id) => {
-        setItems((oldItems) => {
-            return oldItems.filter(e => e.id !== id)
-        })
-    }
+  const deleteItem = (id) => {
+    setItems((oldItems) => {
+      return oldItems.filter(e => e.id !== id)
+    })
+  }
 
 
-    return (
-        <>
-            <div>
-                <h1>Shopping List</h1>
-                <ul>
-                    {items.map(item =>
-                        <li
-                            className="list"
-                            key={item.id}>
-                            {item.product} - {item.quantity}
-                            <button
-                                className="delete"
-                                onClick={() => deleteItem(item.id)}>
-                                x
-                            </button>
-                        </li>)}
-                </ul>
-            </div>
-            <ShoppingListForm onSubmit={addItem} />
-        </>
-    );
+  return (
+    <>
+      <div>
+        <h1>Shopping List</h1>
+        <ul>
+          {items.map(item =>
+            <li
+              className="list"
+              key={item.id}>
+              {item.product} - {item.quantity}
+              <button
+                className="delete"
+                onClick={() => deleteItem(item.id)}>
+                x
+              </button>
+            </li>)}
+        </ul>
+      </div>
+      <ShoppingListForm onSubmit={addItem} />
+    </>
+  );
 }
 
 export default ShoppingList
